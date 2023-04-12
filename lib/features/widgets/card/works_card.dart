d312e199cd0430cdd76badec6a5d6fff86c0301e
denihero <denihero81@gmail.com>
2023-04-12 23:13:20 +0600

    shimmer

--- lib/features/widgets/card/works_card.dart
+++ lib/features/widgets/card/works_card.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:megacom_second_stage/core/network/image_settings.dart';
 import 'package:megacom_second_stage/features/our_works/data/model/our_works_model.dart';
+import 'package:shimmer/shimmer.dart';
 
 class WorksCarouselCard extends StatelessWidget {
   const WorksCarouselCard({Key? key, required this.ourWorks}) : super(key: key);
@@ -17,22 +18,51 @@ class WorksCarouselCard extends StatelessWidget {
         width: 310.w,
         height: 300.h,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                color: const Color(0xFF105BFB).withOpacity(0.3),
-                offset: const Offset(
-                  0,
-                  0.0,
-                ),
-                blurRadius: 1.4,
-                spreadRadius: 1.9,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: const Color(0xFF105BFB).withOpacity(0.3),
+              offset: const Offset(
+                0,
+                0.0,
               ),
-            ],
-            image: DecorationImage(
-                fit: BoxFit.cover,
-                image: CachedNetworkImageProvider(
-                    '${ImageSettings.imageApi}${ourWorks.image}'))),
+              blurRadius: 1.4,
+              spreadRadius: 1.9,
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: CachedNetworkImage(
+            imageUrl: '${ImageSettings.imageApi}${ourWorks.image}',
+            fit: BoxFit.cover,
+              progressIndicatorBuilder: (context, _, __) =>
+                  Shimmer.fromColors(
+                    baseColor: Colors.grey[300]!,
+                    highlightColor: Colors.grey[200]!,
+                    child: Container(
+                        width: 310.w,
+                        height: 300.h,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                          BorderRadius.all(Radius.circular(12)),
+                        ),
+                        child: const Icon(Icons.error)),
+                  ),
+            errorWidget: (context, error, _) => Container(
+                width: 310.w,
+                height: 300.h,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                  ),
+                  borderRadius:
+                  const BorderRadius.all(Radius.circular(12)),
+                ),
+                child: const Icon(Icons.error)),
+          ),
+        ),
       ),
     );
   }
